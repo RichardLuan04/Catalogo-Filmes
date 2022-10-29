ee88007ced2d36ac3506892dde6c51c9470004f5
Richard Luan <richardluan084@gmail.com>
2022-10-28 22:43:55 -0300

    Estilização e funcionamento para pesquisar

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 // URL = https://www.omdbapi.com/?t=[Nome do filme/serie/Anime]&apikey=6c370831
 
 async function Pesquisar_Filme() {
+
     var nomeFilme = document.getElementById('PesquisaFilme').value;
-    var endpoint = `https://www.omdbapi.com/?t=${nomeFilme}&apikey=6c370831`
+    var endpoint = `https://www.omdbapi.com/?t=${nomeFilme}&apikey=6c370831`;
 
     var response = await fetch(endpoint);
     var bodyJson = await response.json();
-
-    var catalogo = document.getElementById("catalogo");
-
-    catalogo.innerHTML = '';
-
-    var imagemJson = bodyJson.Poster
-    var imagem = document.createElement('img')
-    imagem.src = imagemJson
-    catalogo.append(imagem)
+    
+    var imagem = document.getElementById("imagemCatalogo").src = bodyJson.Poster
+    var titulo = document.getElementById("tituloCatalogo").innerText = bodyJson.Title
+    var ano = document.getElementById("anoCatalogo").innerText = bodyJson.Year
+    var tempo = document.getElementById("tempoCatalogo").innerText = bodyJson.Runtime
+    var info = document.getElementById ("infoCatalogo").innerText = bodyJson.Plot
+    var genero = document.getElementById ("generoCatalogo").innerText = bodyJson.Genre
+    var score = document.getElementById("score").innerText = bodyJson.Metascore
+    
+    if (score > 75) {
+        metascore.id= "fundo-verde"
+    }
+    else if (score > 60) {
+        metascore.id= 'fundo-laranja'
+    } else if (score < 60){
+        metascore.id= 'fundo-vermelho'
+    } else {
+        metascore.id= 'fundo-cinza'
+    }
 }
