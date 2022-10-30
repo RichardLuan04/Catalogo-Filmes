7fe3308669739ce51f117637a6548843308dee4a
Richard Luan <richardluan084@gmail.com>
2022-10-29 23:11:28 -0300

    teste

--- script.js
+++ script.js
@@ -10,22 +10,24 @@ async function Pesquisar_Filme() {
     var response = await fetch(endpoint)
     var bodyJson = await response.json()
     
-    var imagem = document.getElementById("imagemCatalogo").src = bodyJson.Poster
-    var titulo = document.getElementById("tituloCatalogo").innerText = bodyJson.Title
-    var ano = document.getElementById("anoCatalogo").innerText = bodyJson.Year
-    var tempo = document.getElementById("tempoCatalogo").innerText = bodyJson.Runtime
-    var info = document.getElementById ("infoCatalogo").innerText = bodyJson.Plot
-    var genero = document.getElementById ("generoCatalogo").innerText = bodyJson.Genre
-    var score = document.getElementById("score").innerText = bodyJson.Metascore
+    if (nomeFilme != ""){
+        var imagem = document.getElementById("imagemCatalogo").src = bodyJson.Poster
+        var titulo = document.getElementById("tituloCatalogo").innerText = bodyJson.Title
+        var ano = document.getElementById("anoCatalogo").innerText = bodyJson.Year
+        var tempo = document.getElementById("tempoCatalogo").innerText = bodyJson.Runtime
+        var info = document.getElementById ("infoCatalogo").innerText = bodyJson.Plot
+        var genero = document.getElementById ("generoCatalogo").innerText = bodyJson.Genre
+        var score = document.getElementById("score").innerText = bodyJson.Metascore
 
-    if (score >= 70) {
-        fundo.setAttribute("id", "fundo-verde") 
-    }
-    else if (score > 60) {
-        fundo.setAttribute("id", "fundo-laranja")
-    } else if (score < 60){
-        fundo.setAttribute("id", "fundo-vermelho")
-    } else {
-        fundo.setAttribute("id", "fundo-cinza")
+        if (score >= 60) {
+            fundo.setAttribute("id", "fundo-verde") 
+        }
+        else if (score >= 40) {
+            fundo.setAttribute("id", "fundo-laranja")
+        } else if (score < 40){
+            fundo.setAttribute("id", "fundo-vermelho")
+        } else {
+            fundo.setAttribute("id", "fundo-cinza")
+        }   
     }
 }
